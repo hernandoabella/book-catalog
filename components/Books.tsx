3878f03d19e8a486322d07e68d7af207f2e1c12b
Hernando Abella <hernandoabella@gmail.com>
2024-05-27 17:22:01 -0500

    Create Books.tsx

---
+++ components/Books.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+    CheatSheets,
+    KeyWords,
+    CookBook,
+    AlgorithmsDataStructures,
+    PatternPrograms,
+    ForBeginners,
+    ProgramsForBeginners,
+    KillerOneLiners,
+    ConceptsEveryDeveloper,
+    AdvancedDesignPatterns,
+    InterviewQuestions,
+    LetsGrowUpTogether,
+  } from "../data";
+
+const Books = () => {
+  return (
+    <div>
+      {/* <CheatSheets />
+      <KeyWords /> */}
+      <CookBook />
+      {/* <AlgorithmsDataStructures />
+      <PatternPrograms />
+      <ForBeginners />
+      <ProgramsForBeginners />
+      <KillerOneLiners />
+      <ConceptsEveryDeveloper />
+      <AdvancedDesignPatterns />
+      <InterviewQuestions />
+      <LetsGrowUpTogether /> */}
+    </div>
+  );
+};
+
+export default Books;
