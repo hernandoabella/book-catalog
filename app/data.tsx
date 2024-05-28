5ec0f15801ef73fcc98a59143226766f5e822dcc
Hernando Abella <hernandoabella@gmail.com>
2024-05-27 23:15:22 -0500

    Update data.tsx

--- app/data.tsx
+++ app/data.tsx
@@ -1,15 +1,15 @@
-import CheatSheets from './components/CheatSheets';
-import KeyWords from './components/KeyWords';
-import CookBook from './components/CookBook';
-import AlgorithmsDataStructures from './components/AlgorithmsDataStructures';
-import PatternPrograms from './components/PatternPrograms';
-import ForBeginners from './components/ForBeginners';
-import ProgramsForBeginners from './components/ProgramsForBeginners';
-import KillerOneLiners from './components/KillerOneLiners';
-import ConceptsEveryDeveloper from './components/ConceptsEveryDeveloper';
-import AdvancedDesignPatterns from './components/AdvancedDesignPatterns';
-import InterviewQuestions from './components/InterviewQuestions';
-import LetsGrowUpTogether from './components/LetsGrowUpTogether';
+import CheatSheets from '@/app/components/CheatSheets';
+import KeyWords from '@/app/components/KeyWords';
+import CookBook from '@/app/components/CookBook';
+import AlgorithmsDataStructures from '@/app/components/AlgorithmsDataStructures';
+import PatternPrograms from '@/app/components/PatternPrograms';
+import ForBeginners from '@/app/components/ForBeginners';
+import ProgramsForBeginners from '@/app/components/ProgramsForBeginners';
+import KillerOneLiners from '@/app/components/KillerOneLiners';
+import ConceptsEveryDeveloper from '@/app/components/ConceptsEveryDeveloper';
+import AdvancedDesignPatterns from '@/app/components/AdvancedDesignPatterns';
+import InterviewQuestions from '@/app/components/InterviewQuestions';
+import LetsGrowUpTogether from '@/app/components/LetsGrowUpTogether';
 
 export {
   CheatSheets,
