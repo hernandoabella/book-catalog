4a7da05e7500e5e0747a59459181d58ca67ad5f2
Hernando Abella <hernandoabella@gmail.com>
2024-05-27 22:58:31 -0500

    Create data.tsx

---
+++ app/data.tsx
@@ -0,0 +1,27 @@
+import CheatSheets from './components/CheatSheets';
+import KeyWords from './components/KeyWords';
+import CookBook from './components/CookBook';
+import AlgorithmsDataStructures from './components/AlgorithmsDataStructures';
+import PatternPrograms from './components/PatternPrograms';
+import ForBeginners from './components/ForBeginners';
+import ProgramsForBeginners from './components/ProgramsForBeginners';
+import KillerOneLiners from './components/KillerOneLiners';
+import ConceptsEveryDeveloper from './components/ConceptsEveryDeveloper';
+import AdvancedDesignPatterns from './components/AdvancedDesignPatterns';
+import InterviewQuestions from './components/InterviewQuestions';
+import LetsGrowUpTogether from './components/LetsGrowUpTogether';
+
+export {
+  CheatSheets,
+  KeyWords,
+  CookBook,
+  AlgorithmsDataStructures,
+  PatternPrograms,
+  ForBeginners,
+  ProgramsForBeginners,
+  KillerOneLiners,
+  ConceptsEveryDeveloper,
+  AdvancedDesignPatterns,
+  InterviewQuestions,
+  LetsGrowUpTogether,
+};
