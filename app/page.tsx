2aa8d6da677e85a8b3080c1ab44a3a4a9c338796
Hernando Abella <hernandoabella@gmail.com>
2024-05-27 20:10:27 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import Image from 'next/image';
 import Books from '@/components/Books';
+import TestimonialWall from '@/components/TestimonialWall';
 
 const Home = () => {
   return (
     <div>
       <div className="relative flex h-screen">
-        <Image
-          src="/bg.avif"
-          alt="image"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"
-          quality={100}
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover"
+          src="/bg.mp4"
+          autoPlay
+          loop
+          muted
         />
         <div className="absolute top-1/2 left-10 max-w-full md:left-20">
           <div className="bg-white text-7xl font-bold text-black mix-blend-screen px-10 py-5">
@@ -21,6 +20,7 @@ const Home = () => {
         </div>
       </div>
       <Books />
+      <TestimonialWall />
     </div>
   );
 };
