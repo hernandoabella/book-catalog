4e5afda93a134c2334cfe553420128adc6651736
Hernando Abella <hernandoabella@gmail.com>
2024-07-06 15:57:01 -0500

    Update Books.tsx

--- app/components/Books.tsx
+++ app/components/Books.tsx
@@ -1,36 +1,23 @@
-import React from "react";
-import {
-  CheatSheets,
-  KeyWords,
-  CookBook,
-  AlgorithmsDataStructures,
-  PatternPrograms,
-  ForBeginners,
-  ProgramsForBeginners,
-  KillerOneLiners,
-  ConceptsEveryDeveloper,
-  AdvancedDesignPatterns,
-  InterviewQuestions,
-  LetsGrowUpTogether,
-} from "../data";
+// CookBooks.tsx
+"use client";
 
-const Books = () => {
+import React from "react";
+import Category from "./Category";
+import { categories } from "./data";  
+
+const CookBooks: React.FC = () => {
   return (
-    <div>
-      {/* <CheatSheets />
-      <KeyWords /> */}
-      <CookBook />
-      <AlgorithmsDataStructures />
-      <PatternPrograms />
-      <ForBeginners />
-      <ProgramsForBeginners />
-      <KillerOneLiners />
-      <ConceptsEveryDeveloper />
-      <AdvancedDesignPatterns />
-      <InterviewQuestions />
-      <LetsGrowUpTogether />
+    <div className="max-w-5xl mx-auto w-full p-10">
+      {categories.map((category, index) => (
+        <div key={index} className="mb-10">
+          <h2 className="text-2xl md:text-3xl mb-5 font-sans font-bold border-l-4 pl-4 border-teal-400 dark:text-gray-200">
+            {category.title}
+          </h2>
+          <Category books={category.books} />
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Books;
+export default CookBooks;
