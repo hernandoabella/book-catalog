6c32af0722ed6a26105fba079cc55a1be5367b77
Hernando Abella <hernandoabella@gmail.com>
2024-07-06 15:57:10 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import Books from '@/app/components/Books';
-import TestimonialWall from '@/app/components/TestimonialWall';
+import React from "react";
+import Header from "@/app/components/Header";
+import Books from "@/app/components/Books";
+import TestimonialWall from "@/app/components/TestimonialWall";
+import Footer from "@/app/components/Footer";
 
 const Home = () => {
   return (
     <div>
-      <div className="relative flex h-screen">
-        <video
-          className="absolute top-0 left-0 w-full h-full object-cover"
-          src="/bg.mp4"
-          autoPlay
-          loop
-          muted
-        />
-        <div className="absolute top-1/2 left-10 max-w-full md:left-20">
-          <div className="bg-white text-7xl font-bold text-black mix-blend-screen px-10 py-5">
-            Book Library
-          </div>
-        </div>
-      </div>
+      <Header />
       <Books />
       <TestimonialWall />
+      <Footer />
     </div>
   );
 };
