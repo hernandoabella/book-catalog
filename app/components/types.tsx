07713aeacc68de5bc5a7e8df3fdb6dbcd88ab590
Hernando Abella <hernandoabella@gmail.com>
2024-07-06 15:57:08 -0500

    Create types.tsx

---
+++ app/components/types.tsx
@@ -0,0 +1,13 @@
+// types.ts
+export interface Book {
+    title: string;
+    link: string;
+    gumroad: string;
+    image: string;
+  }
+  
+  export interface Category {
+    title: string;
+    books: Book[];
+  }
+  
