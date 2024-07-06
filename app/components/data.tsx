f2ba0f1b5ae6521e836d572160a359af554d325e
Hernando Abella <hernandoabella@gmail.com>
2024-07-06 15:57:04 -0500

    Create data.tsx

---
+++ app/components/data.tsx
@@ -0,0 +1,67 @@
+import { Category } from "./types";
+
+export const categories: Category[] = [
+  {
+    title: "Cook Books",
+    books: [
+      {
+        title: "SQL Cook Book",
+        link: "https://www.amazon.com/-/es/Hernando-Abella/dp/B0CWVK8B9R",
+        gumroad: "https://gumroad.com/l/sql-cookbook",
+        image: "/sql-cookbook.jpg",
+      },
+      {
+        title: "JavaScript Cook Book",
+        link: "https://www.amazon.com/Hernando-Abella/dp/B0CPDSXDGL",
+        gumroad: "https://gumroad.com/l/js-cookbook",
+        image: "/js-cookbook.jpg",
+      },
+      {
+        title: "Python Cook Book",
+        link: "https://www.amazon.com/Hernando-Abella/dp/B0CPDSXDGL",
+        gumroad: "https://gumroad.com/l/python-cookbook",
+        image: "/python-cookbook.png",
+      },
+    ],
+  },
+  {
+    title: "250+ Killer One-Liners",
+    books: [
+      {
+        title: "250+ Killer JavaScript One-Liners",
+        link: "https://www.amazon.com/-/es/Hernando-Abella/dp/B0CN58RHGF",
+        gumroad: "https://gumroad.com/l/python-cookbook",
+        image: "/250-killer-js.jpg",
+      },
+      {
+        title: "250+ Killer TypeScript One-Liners",
+        link: "https://www.amazon.com/-/es/Hernando-Abella-ebook/dp/B0CYHZ5QKJ",
+        gumroad: "https://gumroad.com/l/python-cookbook",
+        image: "/250-killer-ts.jpg",
+      },
+      {
+        title: "250+ Killer C# One-Liners",
+        link: "https://www.amazon.com/-/es/Hernando-Abella/dp/B0D31DMBFY",
+        gumroad: "https://gumroad.com/l/python-cookbook",
+        image: "/250-killer-csharp.jpg",
+      },
+      {
+        title: "250+ Killer Ruby One-Liners",
+        link: "https://www.amazon.com/-/es/Hernando-Abella-ebook/dp/B0CXF57XT9",
+        gumroad: "https://gumroad.com/l/python-cookbook",
+        image: "/250-killer-ruby.jpg",
+      },
+    ],
+  },
+  {
+    title: "Data Structures and Algorithms",
+    books: [
+      {
+        title: "Python Cook Book",
+        link: "https://www.amazon.com/Hernando-Abella/dp/B0CPDSXDGL",
+        gumroad: "https://gumroad.com/l/python-cookbook",
+        image: "/python-cookbook.png",
+      },
+    ],
+  },
+];
