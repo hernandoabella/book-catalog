aa0dca6c71bc58d0e78cd6e122f86220b194efa1
Hernando Abella <hernandoabella@gmail.com>
2024-07-06 19:01:39 -0500

    Update types.tsx

--- app/components/types.tsx
+++ app/components/types.tsx
@@ -1,13 +1,17 @@
 // types.ts
 export interface Book {
-    title: string;
-    link: string;
-    gumroad: string;
-    image: string;
-  }
-  
-  export interface Category {
-    title: string;
-    books: Book[];
-  }
-  
+  title: string;
+  link: string;
+  gumroad: string;
+  image: string;
+}
+
+export interface Category {
+  title: string;
+  books: Book[];
+}
+
+export interface FAQ {
+  question: string;
+  answer: string;
+}
