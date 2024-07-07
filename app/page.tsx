e4631dc4d2c3dd460143956cf0fccd9b7ddbcf89
Hernando Abella <hernandoabella@gmail.com>
2024-07-06 19:01:40 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Header from "@/app/components/Header";
 import Books from "@/app/components/Books";
+import NewsletterSection from "@/app/components/NewsletterSection";
 import TestimonialWall from "@/app/components/TestimonialWall";
 import Footer from "@/app/components/Footer";
+import FAQSection from "./components/FAQSection";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Books />
+      <NewsletterSection />
       <TestimonialWall />
+      <FAQSection />
       <Footer />
     </div>
   );
