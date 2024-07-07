a97bc29d6d659c4d260355108ea09dc480691e06
Hernando Abella <hernandoabella@gmail.com>
2024-07-06 20:44:24 -0500

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="p-10 text-center bg-neutral-950 text-white">Footer</div>;
+  return <div className="p-10 text-center bg-neutral-950 text-white">© 2024 CodingBooks. All Rights Reserved.</div>;
 };
 
 export default Footer;
