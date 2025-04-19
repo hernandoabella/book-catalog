42fe1205cda9fe61b73d59f3f9a85f4a6a9b7cc1
Hernando Abella <hernandoabella@gmail.com>
2025-04-19 17:04:28 -0500

    Update Books.tsx

--- app/components/Books.tsx
+++ app/components/Books.tsx
@@ -7,12 +7,14 @@ import { categories } from "./data";
 
 const CookBooks: React.FC = () => {
   return (
-    <div className="max-w-5xl mx-auto w-full p-10">
+    <div className="max-w-5xl mx-auto w-full pt-32">
+      <h1 className="text-2xl md:text-5xl mb-5 font-sans font-bold text-gray-500">Our Book Gallery</h1>
       {categories.map((category, index) => (
         <div key={index} className="mb-10">
-          <h2 className="text-2xl md:text-3xl mb-5 font-sans font-bold border-l-4 pl-4 border-teal-400 dark:text-gray-200">
+          <h2 className="text-2xl md:text-3xl font-sans font-bold border-l-4 pl-4 border-teal-400 dark:text-gray-200">
             {category.title}
           </h2>
+          <p className="text-gray-600 p-5">{category.description}</p>
           <Category books={category.books} />
         </div>
       ))}
