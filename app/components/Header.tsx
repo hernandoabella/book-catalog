94f0a043df11f47b18df43b457291c1c8462c00a
Hernando Abella <hernandoabella@gmail.com>
2025-10-01 14:49:52 -0500

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="bg-neutral-950 text-white p-4 flex items-center justify-between fixed w-full z-50 shadow-xl">
       <a
-        href="https://github.com/hernandoabella/books-catalog"
+        href="https://github.com/hernandoabella/book-catalog"
         target="_blank"
         rel="noopener noreferrer"
         className="text-white text-2xl hover:text-[#93D7DA] transition"
