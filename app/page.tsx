ebe5087fc2f9ea81ae81b4c351534f40ec777987
Hernando Abella <hernandoabella@gmail.com>
2025-10-01 14:46:48 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import Header from "@/app/components/Header";
-import Hero from "@/app/components/Hero";
 import Books from "@/app/components/Books";
-import NewsletterSection from "@/app/components/NewsletterSection";
 import TestimonialWall from "@/app/components/TestimonialWall";
 import Footer from "@/app/components/Footer";
-import FAQSection from "./components/FAQSection";
-import Quotes from "./components/Quotes";
 
 const Home = () => {
   return (
