e71ab4a85c2e5021856d551d08f90e1d304f7ebe
Hernando Abella <hernandoabella@gmail.com>
2025-12-11 11:45:25 -0500

    Update types.tsx

--- app/components/types.tsx
+++ app/components/types.tsx
@@ -1,12 +1,19 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import { ReactNode } from "react";
+
 // types.ts
 export interface Book {
+  [x: string]: string | StaticImport;
+  link: any;
+  gumroad: any;
+  id: string;
   title: string;
-  link: string;
-  gumroad: string;
-  image: string;
+
 }
 
 export interface Category {
+  id: string;
+  icon: ReactNode;
   title: string;
   description: string;
   books: Book[];
